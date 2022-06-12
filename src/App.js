5e74e46f1e872544905f68474482ea2042034811
effiemartins <floramartins182@hotmail.com>
2022-06-12 20:27:43 -0300

    print pokemon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React from "react";
-import { Component } from "react";
 import "./style.css";
 import {DisplayPokemon} from './components/DisplayPokemon';
+import { Component } from "react";
 
-class App extends Components {
+class App extends Component {
     constructor(){
+    super();
 
+    this.state = {
+        pokemons: [],
+            search:''
+        };
     }
 
+    componentDidMount(){
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=500')
+        .then(response => response.json())
+        .then(name => this.setState({pokemons:name.results}));
+    }
+
+    handleChange=(e) => {
+        this.setState({search: e.target.value});
+    };
+
     render(){
+
+        const {pokemons, search } = this.state;
+        const fileteredPokemons = pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase()));
         return(
-            <div class="App"></div>
+            <div class="App">
+                <DisplayPokemon pokemons={fileteredPokemons}></DisplayPokemon>
+            </div>
         )
     }
 }
